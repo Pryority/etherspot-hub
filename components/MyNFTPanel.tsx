0f2e81015d73d52a72c9e253906cdec4b0ff5a70
pryority <matthewapryor@gmail.com>
2022-11-10 16:18:08 -0500

    input bar now updates queryContract state variable on change

--- components/MyNFTPanel.tsx
+++ components/MyNFTPanel.tsx
@@ -1,17 +1,18 @@
+import { NftCollection, NftList, Nft } from 'etherspot';
 import React from 'react';
 
-const MyNFTPanel = ({allNfts}) => {
+const MyNFTPanel = ({nftList}:any) => {
   return (
     <div className='flex flex-col relative'>
-      {allNfts ? 
+      {nftList ? 
         (
           <div className='flex flex-col p-4 absolute top-0 left-0 mx-auto'>
-            {allNfts.map((nft, i) => (
+            {nftList.items.map((collection: NftCollection, index: number) => (
               <div 
-                key={i}
+                key={index}
                 className='p-[3.34px] hover:p-[4.25] hover:bg-green-600/10 hover:pr-8'
               >
-                <h3 className='text-green-900 cursor-pointer transition-all text-sm ease-in-out hover:scale-105 duration-75 hover:pl-8 hover:font-medium p-2 w-full hover:uppercase truncate max-w-[300px]'>{nft.contractName}</h3>
+                <h3 className='text-green-900 cursor-pointer transition-all text-sm ease-in-out hover:scale-105 duration-75 hover:pl-8 hover:font-medium p-2 w-full hover:uppercase truncate max-w-[300px]'>{collection.contractName}</h3>
               </div>
             ))
             }
@@ -21,7 +22,7 @@ const MyNFTPanel = ({allNfts}) => {
         (
           <div className='flex flex-col p-4 absolute top-0 left-0 mx-auto'>
             <div 
-              key={i}
+              key={'test'}
               className='p-[3.34px] hover:p-[4.25] hover:bg-green-600/10 hover:pr-8'
             >
               <h3 className='text-green-900 cursor-pointer transition-all text-sm ease-in-out hover:scale-105 duration-75 hover:pl-8 hover:font-medium p-2 w-full hover:uppercase truncate max-w-[300px]'>Loading...</h3>
--- pages/mainframe.tsx
+++ pages/mainframe.tsx
@@ -1,42 +1,13 @@
-import Head from 'next/head';
-import { Sdk, MetaMaskWalletProvider } from 'etherspot';
+import { Sdk, MetaMaskWalletProvider, NftList, NftCollection, Nft } from 'etherspot';
 import { useEffect, useState } from 'react';
-import { BigNumber, Bytes } from 'ethers';
 import MyNFTPanel from '../components/MyNFTPanel';
 import Navbar from '../components/Navbar';
 
-enum TokenTypes {
-  Erc20 = 'Erc20',
-  Erc721 = 'Erc721',
-  Erc1155 = 'Erc1155',
-  Native = 'Native',
-  WrappedSupertoken = 'WrappedSupertoken'
-}
-
-interface Nft {
-  tokenId: number;
-  name: string;
-  amount: number;
-  image: string;
-  ipfsGateway: string;
-}
-
-interface NftCollection  {
-  contractName: string;
-  contractSymbol: string;
-  contractAddress: string;
-  tokenType: TokenTypes;
-  nftVersion: string;
-  nftDescription: string;
-  balance: number;
-  items: Nft[];
-}
-
 export default function Mainframe() {
-  const [allNfts, setAllNfts] = useState<(NftCollection[])>([]);
+  const [nftList, setNftList] = useState<(NftList | undefined)>();
   const [selectNfts, setSelectNfts] = useState<(Nft[] | undefined)>();
   const [isLoading, setIsLoading] = useState(false);
-  const [queryContract, setQueryContract] = useState('0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85');
+  const [queryContract, setQueryContract] = useState<string | undefined>('0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85');
 
   async function main() {
     if (!MetaMaskWalletProvider.detect()) {
@@ -50,26 +21,37 @@ export default function Mainframe() {
 
     console.info('🛠 ------- SDK created ');
 
-    const output = await sdk.getAccount();
+    const account = await sdk.getAccount();
   
-    console.log('🚹 ------- account', output);
+    console.log('🚹 ------- account', account);
 
-    const list = await sdk.getNftList({
-      account: '0xA7B0BE5E0EC112c72096A43211Db3508D175F454',
-    });
-    
-    setAllNfts(list.items);
+    if (account) {
+      const list = await sdk.getNftList({
+        account: '0xA7B0BE5E0EC112c72096A43211Db3508D175F454',
+      });
 
-    console.log('------- Nft List', {allNfts});
-    const tokens = {allNfts};
-    getCertainNfts();
+      console.log('NFT LIST', list);
+      setNftList(list);
+
+      if (list) {
+        setCollection(list);
+      }
+    }
   }
 
-  const getCertainNfts = async () => {
+  const setCollection = async (list:any) => {
+    if (queryContract != null) {
+      const setList = list.items.filter((nft:NftCollection | undefined) => nft?.contractAddress === queryContract);
+      console.log('SELECTED NFT COLLECTION:', setList[0].contractName);
+      // getCertainNfts(nftList);
+      setSelectNfts(setList[0].items);
+      console.log('SELECTED NFT COLLECTION TOKENS:', setList[0].items);
+    }
+  };
 
-    const contract = allNfts.filter((nft:NftCollection) => nft.contractAddress === queryContract);
-    setSelectNfts(contract[0].items);
-    console.log('A LIST OF A CERTAIN PROJECTs NFTS!', selectNfts);
+  const updateQueryContract = async (e:any) => {
+    setQueryContract(e.target.value);
+    setCollection(queryContract);
   };
 
   useEffect(()=>{
@@ -83,7 +65,7 @@ export default function Mainframe() {
 
       <input
         value={queryContract}
-        onChange={(e) => setQueryContract(e.target.value)}
+        onChange={updateQueryContract}
         type="text"
         placeholder="Enter an NFT Smart Contract Address (eg. 0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85)"
         className={`placeholder:text-gray-600 flex w-5/6 mx-auto text-primary rounded-md h-12 p-2 bg-1 verification-outline border ${queryContract ? 'border-lime-500 border-solid' : 'hover:border-solid hover:border-stone-300'} focus:outline-none`}
@@ -99,31 +81,55 @@ export default function Mainframe() {
           (
             <div className='grid grid-cols-8 w-full h-full'>
               <section className='col-span-3 flex flex-col h-full w-full'>
-                <MyNFTPanel allNfts={allNfts}/>
+                <MyNFTPanel nftList={nftList}/>
               </section>
               {/* PRODUCTS PANEL */}
               <section className='flex flex-col col-span-5 w-full h-full p-4'>
                 <div className='flex flex-col w-full min-h-screen relative'>
                   <div className='flex flex-col p-2 w-full h-full absolute items-center bg-green-600/10 border-2 border-green-700/20'>
                     <div className='grid grid-cols-2 justify-center items-center w-full'>
-                      {selectNfts?.map((nft)=>(
-                        <div key={nft.tokenId} className='flex p-2 justify-center'>
+                      {!nftList ? (
+                        <div className='flex p-2 justify-center'>
                           <div className='flex flex-col h-64 w-64 bg-black/20 relative rounded'>
+                            {/* <Image 
+                            src={nft.image ? nft.image : ''} 
+                            alt=''
+                            // layout='fill absolute'
+                            width={64}
+                            height={64}
+                          /> */}
+                            {/* <img src={nft.image} alt=''/> */}
                             <div className='absolute bottom-2 left-2 flex flex-col space-y-2'>
-                              <h3 className='text-[18px] font-light uppercase'>{nft.name}</h3>
-                              <p>1 ETH</p>
+                              <h3 className='text-[18px] font-light'>Loading...</h3>
+                              <p></p>
                             </div>
                           </div>
                         </div>
-                      ))}
-                      <div className='flex p-2 justify-center'>
-                        <div className='flex flex-col h-64 w-64 bg-black/20 relative rounded'>
-                          <div className='absolute bottom-2 left-2 flex flex-col space-y-2'>
-                            <h3 className='text-[18px] font-light uppercase'>Title</h3>
-                            <p>Price</p>
-                          </div>
-                        </div>
-                      </div>
+                      ):
+                        (
+                          <>
+                            {!selectNfts ? (
+                              <div>
+                                Loading...
+                              </div>
+                            ):
+                              <>
+                                {selectNfts?.map((nft)=>(
+                                  <div key={nft.tokenId} className='flex p-2 justify-center'>
+                                    <div className='flex flex-col h-64 w-64 bg-black/20 relative rounded'>
+                                      <img src={nft.image} alt='' className='hover:scale-105 transitin-all duration-75 ease-in-out'/>
+                                      {/* <div className='absolute bottom-2 left-2 flex flex-col space-y-2'>
+                                        <h3 className='text-[18px] font-light'>{nft.name}</h3>
+                                        <p>1 ETH</p>
+                                      </div> */}
+                                    </div>
+                                  </div>
+                                ))}
+                              </>
+                            }
+                          </>
+                        )
+                      }
                     </div>
                   </div>
                 </div>
