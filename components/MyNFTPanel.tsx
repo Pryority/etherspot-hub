a21618f9644b1536952509efa0c8410af65803a4
pryority <matthewapryor@gmail.com>
2022-11-15 12:08:46 -0500

    update changes

--- components/MyNFTPanel.tsx
+++ components/MyNFTPanel.tsx
@@ -1,26 +1,23 @@
-import { NftCollection, NftList } from 'etherspot';
+import { NftCollection, NftList, Nft, TokenTypes } from 'etherspot';
 import React, { useEffect, useState } from 'react';
 
 interface NFTPanelProps {
   nftList?: NftCollection[];
+  contract?: string;
 }
 
-const MyNFTPanel = ({nftList}:NFTPanelProps) => {
-  const [nftCollections, setNftCollections] = useState<NftCollection[] | undefined>();
-
-  useEffect(()=>{
-    setNftCollections(nftList);
-  });
-
+const MyNFTPanel = ({nftList, contract}:NFTPanelProps) => {
+  
   return (
     <div className='flex flex-col relative'>
       {nftList ? 
         (
           <div className='flex flex-col p-4 absolute top-0 left-0 mx-auto'>
-            {nftCollections?.map((collection: NftCollection, index: number) => (
+            {nftList?.map((collection: NftCollection, index: number) => (
               <div 
                 key={index}
                 className='p-[3.34px] hover:p-[4.25] hover:bg-green-600/10 hover:pr-8'
+                onClick={()=>console.log(`Address: ${collection.contractAddress} Name: ${collection.contractName}`)}
               >
                 <h3 className='text-green-900 cursor-pointer transition-all text-sm ease-in-out hover:scale-105 duration-75 hover:pl-8 hover:font-medium p-2 w-full hover:uppercase truncate max-w-[300px]'>{collection.contractName}</h3>
               </div>
